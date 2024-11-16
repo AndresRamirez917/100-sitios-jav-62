3e356ad985ec5dea6e33cf22b1739967a464ae34
AndresRamirez917 <andresmintic2021@gmail.com>
2024-11-16 15:06:21 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 async function getData() {
-    const result = await fetch('https://fakestoreapi.com/products/');
+    const result = await fetch('https://rickandmortyapi.com/api/character');
     const products = await result.json();
-    const productsArray = products.map(elemento => Object.entries(elemento))
-    console.log(productsArray[8][4])
-    console.log(productsArray[8][4].length)
-    console.log(productsArray)
-    const productsSliced = productsArray.slice(0,1)
-    console.log(products)
-    products.forEach(element => {
+    //console.log(products)
+    const productsArray = products.results.map(elemento => Object.entries(elemento))
+    //console.log(productsArray)
+    //console.log(productsArray[8][4])
+    //console.log(productsArray[8][4].length)
+    //console.log(productsArray)
+    const productsSliced = productsArray.slice(0,3)
+    //console.log(products)
+    products.results.forEach(element => {
         function generarNumeroAleatorio() {
             const min = 9;
             const max = 13;
@@ -15,21 +17,24 @@ async function getData() {
         }
         //const randData = (min, max) => Math.floor(Math.random()*(max - min + 1) + min);
         //const randIndex = randData(1, productsSliced.length)
-        for(i = 2; i < 4; i++){
-            console.log(productsArray[i])
-        if(element.category == "electronics" && element.id >= generarNumeroAleatorio()  && element.id <= generarNumeroAleatorio() ){
-                const box = document.createRange().createContextualFragment(`
-                    
-                    <div class="box box-${i}">
-                        <img src="${element.image}" alt="">
-                        <h3>${element.title}</h3>
-                    </div>
-                    
-                    `)
-                    const project_row = document.querySelector('.project-row');
-                    project_row.append(box)
-            }
-        }
+        //console.log(productsArray[i])
+        //if(element.category == "electronics" && element.id >= generarNumeroAleatorio()  && element.id <= generarNumeroAleatorio() ){
+        for(i = 0; i <= productsSliced.length; i++){
+            if(element.id == i){
+               const box = document.createRange().createContextualFragment(`
+                           
+                           <div class="box box-1">
+                               <img src="${element.image}" alt="">
+                               <h3>${element.name}</h3>
+                           </div>
+                           
+                           `)
+                           const project_row = document.querySelector('.project-row');
+                           project_row.append(box)
+                   }
+           }
+
+        //}
     });
 }
 
